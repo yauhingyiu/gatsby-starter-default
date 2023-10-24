f8704895673310b0f51729dc731b1bdf97f096d6
tamhonwa <yauhingyiu@gmail.com>
2023-10-24 12:13:32 +0800

    Add files via upload

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -8,9 +8,16 @@ const ThirdPage = () => (
   <Layout>
     <h1>Hi from the third page</h1>
 	<ol type="1">
+	<li><a href={`https://news.yahoo.co.jp/pickup/6479425`}>ガザ空爆 1日で子含む436人死亡</a></li>
+	<li><a href="#">広域強盗 指示役の男3人を再逮捕</a></li>
+	<li><a href="#">所得税を定額で4万円減税案 検討</a></li>
+	<li><a href="#">レンタカー15歳遺体 都内で接触か</a></li>
+	<li><a href="#">24歳でドナーに 感謝の手紙は宝物</a></li>
+	<li><a href="#">六甲のおいしい水どこへ 業界軌跡</a></li>
+	<li><a href="#">VIVANT監督 ドラム人気は想定外</a></li>
 	<li><a href="#">山崎まさよし公演 なぜ炎上</a></li>
 	</ol>
-    <Link to="../page-2">Go to page 2</Link>
+    <Link to="page-2">Go to page 2</Link>
   </Layout>
 )
 
