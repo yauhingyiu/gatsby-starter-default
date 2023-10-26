6aea325886a91744c43624dd037159f29ae959d3
tamhonwa <yauhingyiu@gmail.com>
2023-10-26 16:39:09 +0800

    Add files via upload

---
+++ src/components/airtable/readusers.js
@@ -0,0 +1,74 @@
+import * as React from "react"
+import { Link, useStaticQuery, graphql } from 'gatsby'
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+class AirtableReadUsers extends React.Component 
+{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            exchangeRateData: {
+				records:[]
+			}
+        };
+    }
+
+    async componentDidMount() {
+		const response = await fetch('https://api.airtable.com/v0/appMZcSux6RuxBm2i/users', { 
+            method: 'GET', 
+            headers: new Headers({
+                'Authorization': 'Bearer patE8nUiflBpE6FGN.00baa0793d9dc7ccc1a8c3b80f23fa8202fcb3a768aa42379772b924cab31e16', 
+                //'Content-Type': 'application/json'
+            })
+        });
+		const data = await response.json();
+        this.setState({ exchangeRateData: data })
+	}
+
+    render() {
+		
+        const { exchangeRateData } = this.state;
+		const weatherImgStyle = {
+			'background-color':'#1b5397',
+            'border-radius': '15px',
+            'padding': '5px'
+		};
+        return (
+            <div className="card text-center m-3">
+                <h5 className="card-header">Users</h5>
+                <div className="card-body">
+					Time: exchangeRateData.bases<br/>
+                    <table>
+                    <tr>
+						<th>Name</th>
+						<th>Chi Last Name</th>
+						<th>Chi First Name</th>
+						<th>Eng First Name</th>
+						<th>Eng Last Name</th>
+                    </tr>
+                    {
+						exchangeRateData.records.map(
+							(base, i) => (
+								
+								<tr>
+                                    <td>{base.fields['Name']}</td>
+                                    <td>{base.fields['Chi Last Name']}</td>
+                                    <td>{base.fields['Chi First Name']}</td>
+                                    <td>{base.fields['Eng First Name']}</td>
+                                    <td>{base.fields['Eng Last Name']}</td>
+                                </tr>
+							)
+						)
+					}
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AirtableReadUsers; 
+
---
+++ src/components/airtable/saveusers.js
@@ -0,0 +1,125 @@
+import * as React from "react"
+import { Link, useStaticQuery, graphql } from 'gatsby'
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+
+class AirtableSaveUsers extends React.Component 
+{
+    constructor(props) {
+		super(props);
+		this.state = {
+			name: 'aa',
+			chiFirstName: 'bb',
+			chiLastName: 'cc',
+			engFirstName: 'dd',
+			engLastName: 'ee',
+			postResponseJson: {},
+			postResponse: 0
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+		this.setState({
+			[name]: value
+		});
+	}
+
+	handleSubmit(event) {
+		console.log('name ', this.state.name, 
+			'chi name', this.state.chiFirstName,
+			'chi name', this.state.chiLastName,
+			'eng name', this.state.engFirstName,
+			'eng name', this.state.engLastName
+		);
+		event.preventDefault();
+		this.postData();
+	}
+	
+	async postData() {
+		console.log( JSON.stringify({
+			"records": [
+			{
+				"fields": {
+					'Name': this.state.name,
+					'Chi First Name': this.state.chiFirstName,
+					'Chi Last Name': this.state.chiLastName,
+					'Eng First Name': this.state.engFirstName,
+					'Eng Last Name': this.state.engLastName
+				}
+			}
+			]}) );
+		const response = await fetch('https://api.airtable.com/v0/appMZcSux6RuxBm2i/users', { 
+            method: 'POST', 
+            headers: new Headers({
+                'Authorization': 'Bearer patiUNqn39Bm9TZc9.17ceec11827dedb7d6905f18c893d8d645da839d70334eddb3ae1eef003916bd', 
+                'Content-Type': 'application/json'
+            }),
+			body: JSON.stringify({
+			"records": [
+			{
+				"fields": {
+					name: this.state.name,
+					chiFirstName: this.state.chiFirstName,
+					chiLastName: this.state.chiLastName,
+					engFirstName: this.state.engFirstName,
+					engLastName: this.state.engLastName
+				}
+			}
+			]})
+        });
+		const data = await response.json();
+        this.setState({ 
+			postResponseJson: data,
+			postResponse: response.status*1
+		})
+	}
+
+	render() {
+		return (
+		<form onSubmit={this.handleSubmit}>
+		<label>
+		<div>
+		{
+			(this.state.postResponse>0)? (this.state.postResponse==200?'Saved':'Error occured'):''
+		}
+		</div>
+		</label>
+		<br/>
+		<label>
+		Name:
+		<input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+		</label>
+		<label>
+		Chi First Name:
+		<input type="text" name="chiFirstName" value={this.state.chiFirstName} onChange={this.handleChange} />
+		</label>
+		<label>
+		Chi Last Name:
+		<input type="text" name="chiLastName" value={this.state.chiLastName} onChange={this.handleChange} />
+		</label>
+		<label>
+		Eng First Name:
+		<input type="text" name="engFirstName" value={this.state.engFirstName} onChange={this.handleChange} />
+		</label>
+		<label>
+		Eng Last Name:
+		<input type="text" name="engLastName" value={this.state.engLastName} onChange={this.handleChange} />
+		</label>
+		<label>
+		<input type="submit" value="Submit" />
+		</label>
+		
+		</form>
+		);
+	}
+}
+
+export default AirtableSaveUsers; 
+
