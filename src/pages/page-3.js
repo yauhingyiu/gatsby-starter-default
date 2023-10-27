07c100dfcef4bb0cd2299561da2ac601d10e7108
tamhonwa <yauhingyiu@gmail.com>
2023-10-27 14:40:42 +0800

    Add files via upload

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo"
 
 const ThirdPage = () => {
 	
-	const siteMetadata = () => {
+	const useSiteMetadata = () => {
 		const {site} = useStaticQuery(graphql`
 			query {
 				site {
@@ -24,10 +24,11 @@ const ThirdPage = () => {
 		return site.siteMetadata
 	}
   
+	const { title, airtable } = useSiteMetadata();
 
-  return (
+	return (
   <Layout>
-    <h1>Hello from the third page - {siteMetadata.title}</h1>
+    <h1>Hello from the third page - {title}</h1>
 	<ol type="1">
 	<li><a href={`https://news.yahoo.co.jp/pickup/6479425`}>ガザ空爆 1日で子含む436人死亡</a></li>
 	<li><a href="#">広域強盗 指示役の男3人を再逮捕</a></li>
