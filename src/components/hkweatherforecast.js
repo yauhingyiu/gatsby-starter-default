88f325562196b47e633e76252d5a77c228c475b4
tamhonwa <yauhingyiu@gmail.com>
2023-10-30 11:39:06 +0800

    Add files via upload

--- src/components/hkweatherforecast.js
+++ src/components/hkweatherforecast.js
@@ -4,48 +4,47 @@ import { Link, useStaticQuery, graphql } from 'gatsby'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-class HkWeatherForecast extends React.Component 
+const HkWeatherForecast = ({props, children}) =>
 {
-    constructor(props) {
-        super(props);
+	const [weatherData, setWeatherData] = useState({
+        weatherForecast:[]
+    }); 
+	
+	useEffect(() => { 
+        console.log('Component mounted'); 
+        componentDidMount();
+    }, []); 
 
-        this.state = {
-            weatherData: {
-				weatherForecast:[]
-			}
-        };
-    }
-
-    async componentDidMount() {
+    const componentDidMount = async () => {
 		// GET request using fetch with async/await
 		const response = await fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=tc');
 		const data = await response.json();
-		this.setState({ weatherData: data })
+		setWeatherData( data );
 	}
 
-    render() {
-        const { weatherData } = this.state;
-		const weatherImgStyle = {
-			'background-color':'#1b5397',
-            'border-radius': '15px',
-            'padding': '5px'
-		};
-        return (
-            <div className="card text-center m-3">
-                <h5 className="card-header">GET Request with Async/Await</h5>
-                <div className="card-body">
-					{weatherData.generalSituation}
-                    {
-                        weatherData.weatherForecast.map(
-							(link, i) => (
-                            <div>{link.forecastDate}<br/>{link.forecastWeather}<br/><img style={weatherImgStyle} width="77px" src={`https://www.hko.gov.hk/images/HKOWxIconOutline/pic${link.ForecastIcon}.png`} border="0"/><br/></div>
-                            )
+
+	const weatherImgStyle = {
+		'background':'#1b5397',
+		'border-radius': '15px',
+		'padding': '5px'
+	};
+	
+	return (
+		<div className="card text-center m-3">
+			<h5 className="card-header">GET Request with Async/Await</h5>
+			<div className="card-body">
+				{weatherData.generalSituation}
+				{
+					weatherData.weatherForecast.map(
+						(link, i) => (
+						<div>{link.forecastDate}<br/>{link.forecastWeather}<br/><img style={weatherImgStyle} width="77px" src={`https://www.hko.gov.hk/images/HKOWxIconOutline/pic${link.ForecastIcon}.png`} border="0"/><br/></div>
 						)
-                    }
-                </div>
-            </div>
-        );
-    }
+					)
+				}
+			</div>
+		</div>
+	);
+
 }
 
 export default HkWeatherForecast; 
